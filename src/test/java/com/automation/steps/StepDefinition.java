2ad9c171e8ad61d43b457bbe2d40b7957b5f47dc
Basanov Maxim <basanovmax@gmail.com>
2023-05-15 21:59:51 -0700

    cucumber new

--- src/test/java/com/automation/steps/StepDefinition.java
+++ src/test/java/com/automation/steps/StepDefinition.java
@@ -44,32 +44,41 @@ public class StepDefinition {
 
 	@When("click on cart icon")
 	public void click_on_cart_icon() {
-
 	}
 
 	@Then("verify user is on cart page")
 	public void verify_user_is_on_cart_page() {
-
 	}
 
 	@Then("verify item details on the cart page")
 	public void verify_item_details_on_the_cart_page() {
-
 	}
 
 	@When("user click on continue shopping button")
 	public void user_click_on_continue_shopping_button() {
-
 	}
 
 	@When("user clicks on remove button of item")
 	public void user_clicks_on_remove_button_of_item() {
-
 	}
 
 	@Then("verify item is removed from the cart")
 	public void verify_item_is_removed_from_the_cart() {
-
 	}
 
+
+
+	@Then("verify user can not log in on home page")
+	public void verify_user_can_not_log_in_on_home_page() {
+	}
+
+	@When("user enters username {string} and password {string}")
+	public void userEntersUsernameAndPassword(String username, String password) {
+		System.out.println("Login with username " + username);
+		System.out.println("Login with password " + password);
+	}
+
+	@Then("verify user could not access home page")
+	public void verifyUserCouldNotAccessHomePage() {
+	}
 }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,7 +1,25 @@
 Feature: Verify Login Feature
 
-  Scenario: Verify user can login with valid credentials
+  Background:
     Given user opens the website
     And verify user is on login page
-    When user enters valid credentials
+
+  @smoke @chirag
+  Scenario Outline: Verify user can login with valid credentials
+    When user enters username "<username>" and password "<password>"
     Then verify user is on home page
+    Examples:
+      | username | password |
+      | demo     | demo@123 |
+      |          |          |
+      | ADMIN    | ADMIN@123|
+      | admin    | demo@123 |
+      | demo     | admin@123|
+
+  @regression
+  Scenario: Verify user can login with invalid credentials
+    When user enters username "demo" and password "demo@123"
+    Then verify user could not access home page
+
+
+
